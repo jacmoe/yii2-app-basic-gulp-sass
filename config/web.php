ccb6312cf5cca2c951a503e982f0f2384eb32ce1
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-08 07:33:08 +0100

    Pretty URLs on

--- config/web.php
+++ config/web.php
@@ -41,14 +41,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
